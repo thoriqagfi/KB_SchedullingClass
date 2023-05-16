411563b21a30047701fbe91ae615428733d25d79
Urdhanaka Aptanagi <46347836+urdhanaka@users.noreply.github.com>
2023-05-16 14:37:24 +0700

    fix: next button

--- interface.py
+++ interface.py
@@ -12,20 +12,6 @@ class CourseSchedulerApp:
         self.root = root
         self.root.title("Course Scheduler Informatics Engineering")
 
-        self.data = [
-            {'Course': 'CSE102', 'Day': 'Wednesday', 'Time': '10:00-11:40'},
-            {'Course': 'CSE401', 'Day': 'Monday', 'Time': '10:00-11:40'},
-            {'Course': 'CSE101', 'Day': 'Friday', 'Time': '8:00-9:40'}
-        ]
-
-        # Create a Treeview widget
-        self.tree = ttk.Treeview(root)
-        self.tree["columns"] = ("course name", "day", "time")
-        self.tree.heading("#0", text="Course")
-        self.tree.heading("course name", text="Course Name")
-        self.tree.heading("day", text="Day")
-        self.tree.heading("time", text="Time")
-
         # Komponen GUI
         self.label = tk.Label(root, text="Course List:")
         self.label.pack()
@@ -37,55 +23,51 @@ class CourseSchedulerApp:
 
         self.page = 0
 
-        # Tambahkan pilihan mata kuliah ke dalam Listbox
-        # for i in self.data:
-        #     self.course_list.insert(tk.END, i['Course'])
-
-        self.course_list.insert(tk.END, "Data Structures and Algorithms")
-        self.course_list.insert(tk.END, "Artificial Intelligence")
-        self.course_list.insert(tk.END, "Introduction to Computer Science")
-        self.course_list.insert(tk.END, "Operating Systems")
-        self.course_list.insert(tk.END, "Database Systems")
-        self.course_list.insert(tk.END, "Computer Networks")
-        self.course_list.insert(tk.END, "Web Programming")
-        self.course_list.insert(tk.END, "Software Engineering")
-        self.course_list.insert(tk.END, "Computer Graphics")
-        self.course_list.insert(tk.END, "Internet Security")
-        self.course_list.insert(tk.END, "Parallel and Distributed Computing")
-        self.course_list.insert(tk.END, "Data Mining and Knowledge Discovery")
+        for course_name in self.courses_id_dict.values():
+            self.course_list.insert(tk.END, course_name)
 
         self.start_button = tk.Button(root, text="Start", command=self.start_genetic_algorithm)
         self.start_button.pack()
 
-    def insert_to_table(self, result):
+        # Create a Treeview widget
+        self.tree = ttk.Treeview(root)
+        self.tree["columns"] = ("course name", "day", "time")
+        self.tree.heading("#0", text="Course")
+        self.tree.heading("course name", text="Course Name")
+        self.tree.heading("day", text="Day")
+        self.tree.heading("time", text="Time")
+        self.tree.pack()
+
+        self.next_button = tk.Button(root, text="Next")
+        self.next_button.pack()
+
+        # Tambahkan pilihan mata kuliah ke dalam Listbox
+        # for i in self.data:
+        #     self.course_list.insert(tk.END, i['Course'])
+
+    def insert_to_table(self):
         for i in self.tree.get_children():
             self.tree.delete(i)
 
-        for data in result[self.page]:
+        for data in self.result[self.page]:
             course = data
             course_name = self.courses_id_dict[data]
-            day = result[self.page][data][0]
-            time = result[self.page][data][1]
+            day = self.result[self.page][data][0]
+            time = self.result[self.page][data][1]
             self.tree.insert("", "end", text=course, values=(course_name, day, time))
 
-        self.tree.pack()
-
-    def next_page(self, result):
+    def next_page(self):
         self.page += 1
-        self.insert_to_table(result)
-
-    def next_page_button(self, result):
-        self.next_button = tk.Button(root, text="Next", command=self.next_page(result))
-        self.next_button.pack()
+        self.insert_to_table()
 
     def start_genetic_algorithm(self):
         selected_courses = [self.course_list.get(i) for i in self.course_list.curselection()]
         if len(selected_courses) < 2:
             messagebox.showerror("Error", "Please select at least 2 courses.")
         else:
-            result = genetic_algorithm(selected_courses)
-            self.insert_to_table(result)
-            self.next_page_button(result)
+            self.result = genetic_algorithm(selected_courses)
+            self.insert_to_table()
+            self.next_button.configure(command=self.next_page)
 
 if __name__ == "__main__":
     root = tk.Tk()
