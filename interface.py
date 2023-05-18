69c5d34c21ebca163feb885df603ef36ee35593f
Urdhanaka Aptanagi <46347836+urdhanaka@users.noreply.github.com>
2023-05-18 14:23:10 +0700

    feat: add comment

--- interface.py
+++ interface.py
@@ -1,11 +1,8 @@
-from tkinter import *
 import tkinter as tk
+from tkinter import *
 from tkinter import ttk
-from ClassScheduling import *
-from datetime import datetime
-import random
 from tkinter import messagebox
-
+from ClassScheduling import *
 
 class CourseSchedulerApp:
     def __init__(self, root):
@@ -19,10 +16,14 @@ class CourseSchedulerApp:
         self.course_list = tk.Listbox(root, selectmode=tk.MULTIPLE)
         self.course_list.pack()
 
+        # dictionary courses dari ClassScheduling.py
         self.courses_id_dict = courses_id_dict
 
+        # Penunjuk halaman pada saat output hasil GA
         self.page = 0
 
+        # Memasukkan hasil dictionary ke list
+        # yang akan dipilih oleh user
         for course_name in self.courses_id_dict.values():
             self.course_list.insert(tk.END, course_name)
 
@@ -38,14 +39,14 @@ class CourseSchedulerApp:
         self.tree.heading("time", text="Time")
         self.tree.pack()
 
-        self.next_button = tk.Button(root, text="Next")
+        self.next_button = tk.Button(root, text="Next", command=self.next_page)
         self.next_button.pack()
 
-        # Tambahkan pilihan mata kuliah ke dalam Listbox
-        # for i in self.data:
-        #     self.course_list.insert(tk.END, i['Course'])
-
     def insert_to_table(self):
+        '''
+        Metode untuk memasukkan hasil Genetic Algorithm ke dalam
+        tabel treeview
+        '''
         for i in self.tree.get_children():
             self.tree.delete(i)
 
@@ -57,6 +58,10 @@ class CourseSchedulerApp:
             self.tree.insert("", "end", text=course, values=(course_name, day, time))
 
     def next_page(self):
+        '''
+        Metode untuk menampilkan hasil lain dari Genetic Algorithm
+        dengan menggunakan 'page'
+        '''
         if self.page < len(self.result) - 1:
             self.page += 1
         else:
@@ -64,13 +69,16 @@ class CourseSchedulerApp:
         self.insert_to_table()
 
     def start_genetic_algorithm(self):
+        '''
+        Metode untuk memulai Genetic Algorithm berdasarkan
+        pilihan course user
+        '''
         selected_courses = [self.course_list.get(i) for i in self.course_list.curselection()]
         if len(selected_courses) < 2:
             messagebox.showerror("Error", "Please select at least 2 courses.")
         else:
             self.result = genetic_algorithm(selected_courses)
             self.insert_to_table()
-            self.next_button.configure(command=self.next_page)
 
 if __name__ == "__main__":
     root = tk.Tk()
